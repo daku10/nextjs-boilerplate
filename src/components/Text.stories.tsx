5553de164aa6f0205e4f783e918774a55e13d891
daku10 <daku.two@gmail.com>
2021-10-02 23:20:38 +0900

    update CSF

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,7 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { Text } from './Text'
 
 export default {
   title: 'Text',
-}
+  component: Text,
+  argTypes: {
+    text: {
+      control: { type: 'text' },
+    },
+  },
+} as ComponentMeta<typeof Text>
 
-export const text = () => <Text text="Hello storybook" />
+const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />
+
+export const HelloText = Template.bind({})
+HelloText.args = {
+  text: 'hello!',
+}
